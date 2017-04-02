95b5182e115c1400be17dacd4817d1b364d86022
gleifvo <77hagor777@gmail.com>
2017-04-02 21:12:20 +0300

    feat(security): Simple in-memory implementaion

--- src/main/java/timetracking/configurations/WebSecurityConfig.java
+++ src/main/java/timetracking/configurations/WebSecurityConfig.java
@@ -1,28 +1,31 @@
-//package timetracking.configurations;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest()
-//                .permitAll();
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication();
-//    }
-//
-//}
+package timetracking.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("string2").password("string2").roles("ADMIN")
+                .and()
+                .withUser("string").password("string").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.httpBasic();
+        http.csrf().disable();
+    }
+
+}
--- src/main/java/timetracking/dao/repositories/UserRepository.java
+++ src/main/java/timetracking/dao/repositories/UserRepository.java
@@ -2,10 +2,16 @@ package timetracking.dao.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PostAuthorize;
 import timetracking.dao.models.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
+    @PostAuthorize("returnObject.login == principal.username or hasRole('ADMIN')")
     User findByLogin(@Param("login") String login);
+
+    @Override
+    @PostAuthorize("returnObject.login == principal.username or hasRole('ADMIN')")
+    User findOne(Long aLong);
 
 }
