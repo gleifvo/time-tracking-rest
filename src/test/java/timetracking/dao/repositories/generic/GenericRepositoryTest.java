c0a9469896d89d427973f1ba8637f056d8e090df
gleifvo <77hagor777@gmail.com>
2017-04-03 21:54:19 +0300

    test(*): Add mock user for tests

--- src/test/java/timetracking/dao/repositories/generic/GenericRepositoryTest.java
+++ src/test/java/timetracking/dao/repositories/generic/GenericRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import timetracking.dao.models.AbstractEntity;
@@ -18,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
+@WithMockUser(roles = {"ADMIN"})
 public abstract class GenericRepositoryTest<REPOSITORY extends CrudRepository<ENTITY, Long>, ENTITY extends AbstractEntity> {
 
     @Autowired
